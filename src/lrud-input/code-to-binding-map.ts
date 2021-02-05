afe813fd46731365dc511ec4ea415bc9aa643b2b
Michael Groh <michael@mgroh.net>
2021-02-05 22:20:58 +0100

    Use 'keycode' instead of 'key' to map KeyboardEvents to bindings
    Map webos back keycode

--- src/lrud-input/code-to-binding-map.ts
+++ src/lrud-input/code-to-binding-map.ts
@@ -1,8 +1,10 @@
 // This maps a key code, returned from an event, to a handler name.
+// TODO: add configuration of custom key map
 export default {
   38: 'up',
   40: 'down',
   37: 'left',
   39: 'right',
   13: 'select',
+  461: 'back' // webos remote
 };
