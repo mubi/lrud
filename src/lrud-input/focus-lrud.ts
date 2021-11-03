abe486dca3b58e2aa5040a149c5d597a8078d43f
Simon Tsang <84047+simontsang@users.noreply.github.com>
2021-11-03 14:44:00 +0000

    Use `.keyCode` only as a fallback to `.key`

--- src/lrud-input/focus-lrud.ts
+++ src/lrud-input/focus-lrud.ts
@@ -1,5 +1,6 @@
 import throttle from './throttle';
-import keyToBindingMap from './code-to-binding-map';
+import keyCodeToBindingMap from './code-to-binding-map';
+import keyToBindingMap from './key-to-binding-map';
 import { FocusStore } from '../types';
 import bubbleKey from './bubble-key-input';
 
@@ -32,8 +33,12 @@ export default function focusLrud(focusStore: FocusStore) {
 
   const keydownHandler = throttle(
     function (e: KeyboardEvent) {
-      // @ts-ignore
-      const bindingName = keyToBindingMap[e.keyCode];
+      const bindingName: string = e.key
+        // @ts-ignore
+        ? keyToBindingMap[e.key]
+        // @ts-ignore
+        : keyCodeToBindingMap[e.keyCode];
+
       // @ts-ignore
       const binding = lrudMapping[bindingName];
 
